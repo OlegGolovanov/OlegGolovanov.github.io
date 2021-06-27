139fc8594acaec655002672dabb07cc3395405fa
Oleg <olegjan9@rambler.ru>
2021-06-27 14:48:53 +0300

    27.06.2021

--- js/script.js
+++ js/script.js
@@ -210,3 +210,210 @@ document.querySelector(".windows").addEventListener("click", (e) => {
     hideModals();
   }
 });
+
+// Валидация форм
+
+const forms = document.querySelectorAll("form"),
+  regularEmail = /^[^@]+@[^@.]+\.[^@]+$/,
+  regularUser = /^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$/,
+  regularTel = /^((\+?7|8)[ \-] ?)?((\(\d{3}\))|(\d{3}))?([ \-])?(\d{3}[\- ]?\d{2}[\- ]?\d{2})$/;
+// Переменные для проверки на зполненность и валидированность полей
+let validateEmail,
+  validateUser,
+  validateTel;
+
+
+
+
+forms.forEach((form, i) => {
+
+
+  // Валидация форм
+  form.querySelectorAll("input").forEach(input => {
+    if (!input.classList.contains(".btn")) {
+      if (input != "") {
+        input.addEventListener('blur', () => {
+          validateInput(input);
+        });
+      }
+    }
+  });
+
+  const validateInput = (input) => {
+    if (input.name == "name") {
+      // Обращаемся к input name. Проверка имени пользователя      
+      if (!regularUser.test(input.value) &&
+        input.value != '') {
+        // если не соответствует эталону (регулярному выражению) то
+        // проверка на валидность && и валидность проверяется, если 
+        // не пустая строка. Последнее, чтобы сообщение об ошибке не
+        // появлялось при загр. страницы.          
+        input.nextElementSibling.textContent = "Не корректное ФИО";
+        input.classList.add("form-feed__input__error");
+        input.classList.remove("form-feed__input__not-error");
+        validateUser = false;
+      } else {
+        input.nextElementSibling.textContent = "";
+        // если дозаполняем, то текст ошибки удаляется
+        input.classList.remove("form-feed__input__error");
+        input.classList.add("form-feed__input__not-error");
+        validateUser = true;
+      }
+    }
+
+    if (input.name == "email") {
+      if (!regularEmail.test(input.value) &&
+        input.value != "") {
+        input.nextElementSibling.textContent = "Не корректный email";
+        input.classList.add("form-feed__input__error");
+        input.classList.remove("form-feed__input__not-error");
+        validateEmail = false;
+      } else {
+        input.nextElementSibling.textContent = "";
+        // если дозаполняем, то текст ошибки удаляется
+        input.classList.remove("form-feed__input__error");
+        input.classList.add("form-feed__input__not-error");
+        validateEmail = true;
+      }
+    }
+
+    if (input.name == 'phone') {
+      if (!regularTel.test(input.value) &&
+        input.value != "") {
+        input.nextElementSibling.textContent = "Не корректный номер телефона";
+        input.classList.add("form-feed__input__error");
+        input.classList.remove("form-feed__input__not-error");
+        validateTel = false;
+      } else {
+        input.nextElementSibling.textContent = "";
+        // если дозаполняем, то текст ошибки удаляется
+        input.classList.remove("form-feed__input__error");
+        input.classList.add("form-feed__input__not-error");
+
+        validateTel = true;
+      }
+    }
+  };
+
+  // Маска номера телефона
+  const MaskTel = function () {
+
+    var phoneInputs = form.querySelectorAll('[data-tel]');
+
+    var getInputNumbersValue = function (input) {
+      // Return stripped input value — just numbers
+      return input.value.replace(/\D/g, '');
+    }
+
+    var onPhonePaste = function (e) {
+      var input = e.target,
+        inputNumbersValue = getInputNumbersValue(input);
+      var pasted = e.clipboardData || window.clipboardData;
+      if (pasted) {
+        var pastedText = pasted.getData('Text');
+        if (/\D/g.test(pastedText)) {
+          // Attempt to paste non-numeric symbol — remove all non-numeric symbols,
+          // formatting will be in onPhoneInput handler
+          input.value = inputNumbersValue;
+          return;
+        }
+      }
+    }
+
+    var onPhoneInput = function (e) {
+      var input = e.target,
+        inputNumbersValue = getInputNumbersValue(input),
+        selectionStart = input.selectionStart,
+        formattedInputValue = "";
+
+      if (!inputNumbersValue) {
+        return input.value = "";
+      }
+
+      if (input.value.length != selectionStart) {
+        // Editing in the middle of input, not last symbol
+        if (e.data && /\D/g.test(e.data)) {
+          // Attempt to input non-numeric symbol
+          input.value = inputNumbersValue;
+        }
+        return;
+      }
+
+      if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+        if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+        var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+        formattedInputValue = input.value = firstSymbols + " ";
+        if (inputNumbersValue.length > 1) {
+          formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+        }
+        if (inputNumbersValue.length >= 5) {
+          formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+        }
+        if (inputNumbersValue.length >= 8) {
+          formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+        }
+        if (inputNumbersValue.length >= 10) {
+          formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+        }
+      } else {
+        formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+      }
+      input.value = formattedInputValue;
+    }
+    var onPhoneKeyDown = function (e) {
+      // Clear input after remove last symbol
+      var inputValue = e.target.value.replace(/\D/g, '');
+      if (e.keyCode == 8 && inputValue.length == 1) {
+        e.target.value = "";
+      }
+    }
+    for (var phoneInput of phoneInputs) {
+      phoneInput.addEventListener('keydown', onPhoneKeyDown);
+      phoneInput.addEventListener('input', onPhoneInput, false);
+      phoneInput.addEventListener('paste', onPhonePaste, false);
+    }
+
+  };
+
+  MaskTel();
+
+
+  // Проверка на заполненность форм  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.querySelectorAll("input").forEach(input => {
+      if (input.tagName != "BUTTON") {
+        // отсекаем ненужные элементы, в данном случае checkbox и button
+        if (input.value == "") {
+          // и если оставшиеся элементы не заполнены
+          input.nextElementSibling.textContent = "Данное поле не заполнено!";
+          input.classList.add("form-feed__input__error");
+          input.classList.remove("form-feed__input__not-error");
+
+          // то у этого незаполненного элемента сосед nextElementSibling 
+          // будет показывать указанный текст
+          // в верстке под каждым инпутом добавить div, в который и будет
+          // записываться этот текст          
+        } else {
+          input.nextElementSibling.textContent = "";
+          // если дозаполняем, то текст ошибки удаляется 
+          input.classList.remove("form-feed__input__error");
+          input.classList.add("form-feed__input__not-error");
+          if (validateEmail &&
+            validateUser &&
+            validateTel) {
+            alert("Форма отправлена");
+            form.reset();
+            hideModals();
+            form.removeEventListener("submit", (e));
+          }
+        }
+      }
+    });
+
+  });
+
+
+
+
+});
